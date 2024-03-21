93ba97e2eed08008dbb0edcbfe1a05fea0adb898
H@mer <13221018+chaixshot@users.noreply.github.com>
2024-03-21 08:26:54 +0700

    Languages font support
    - Arabic
    - Chinese'

--- config.lua
+++ config.lua
@@ -7,3 +7,10 @@ Config.Color = {r=0, g=255, b=128, a=200}
 Config.Location = {x = 200.236, y=-994.613, z=29.092}
 Config.Blip = 673
 Config.BlipColor = 83
+
+-- Custom font support languages.
+-- Uncomment to enable font.
+CreateThread(function()
+    -- RegisterFontFile('arabic')
+    -- RegisterFontFile('chinese')
+end)
